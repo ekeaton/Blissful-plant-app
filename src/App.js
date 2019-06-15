6bf4154032870cd60d50f287dc756d9794ea8a31
Estelle K <e.keaton@live.com>
2019-06-15 10:45:05 -0700

    added styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
+import NavBar from './NavBar/NavBar';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        <img src='https://i.imgur.com/GHlSnmo.png' className="App-logo" alt="logo" />
-        </a>
-      </header>
+     <NavBar/>
       <main>
         
       </main>
 
     </div>
   );
+ }
 }
 
 export default App;
